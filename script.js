c50a837c3141499ed120df369c0f04c18e4915ec
alebusi <allebusi@gmail.com>
2021-06-08 16:44:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-var intestazione='<svg viewBox="-10 -10 120 120"> <path d=\"M0 0';
+var intestazione='<svg viewBox="-10 -10 120 120">';
 var coda="\" /></svg>";
+var griglia='<path id="griglia" d=\"M0 0 h100 m0 0 v100 m0 0 h-100 m0 0 v-100 m25 0 v100 M50 0 v100 M75 0 v100 M0 25 h100 M0 50 h100 M0 75 h100"/>';
+var nPath='<path d=\"M 0 0';
 var testo="";
 var pos=0;
 var cursore="_";
@@ -11,12 +13,12 @@ function scriviTxt(car) {
     testo=testo.substring(0,pos) + testo.substring(pos+1);
   }
   else if (car == '<') {
-    testo=intestazione+testo;
-    pos+=intestazione.length;
+    testo=testo+'"/>'+nPath;
+    pos=testo.length;
   }
   else if (car == ">") {
-    testo=testo+coda;
-    pos+=coda.length;
+    testo=intestazione+testo+coda;
+    pos=testo.length;
   }
   else if (car == "O") {
     cerchio();
@@ -28,7 +30,9 @@ function scriviTxt(car) {
     pos+=1;
   }
   document.getElementById("testo").value = testo;
-  testoAnteprima=intestazione+" "+testo.replace(intestazione,"").replace(coda,"")+coda;
+  testoAnteprima=intestazione+" "+griglia+" "+nPath+" "+
+                 testo.replace(intestazione,"").replace(coda,"")+coda;
+  //alert(testoAnteprima);
   document.getElementById("anteprima").innerHTML=testoAnteprima;
   //mySelect(pos);  
 }
